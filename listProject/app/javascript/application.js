e6b932f772ef7740b62b3bb3989ac293fa0ccf5c
Emma-Abigail Gachagua <em.gachagua@baypath.net>
2022-12-13 19:21:52 +0000

    Custom Js file/Select list

--- listProject/app/javascript/application.js
+++ listProject/app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import "./custom/myscripts"
+
+
+import jquery from 'jquery'
+window.jQuery = jquery
+window.$ = jquery
